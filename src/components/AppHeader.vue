766b635adca0322545e72a0fe8b4cde95f4415c8
joe.lin <joe.lin@silkrode.com.tw>
2020-04-15 18:18:10 +0800

    add: AppHeader test button show or not

---
+++ src/components/AppHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="header-wrapper">
+    <button v-show="isLogin">Logout</button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AppHeader",
+  data() {
+    return {
+      isLogin: false
+    };
+  }
+};
+</script>
